da62ec770072689aefe20d10b0224a4eeeb9343d
Abdullah <abdullahdeveloper843@gmail.com>
2025-07-18 17:49:30 +0500

    CORS errors are being resolved

--- server/middlewares/modules/cors.middleware.js
+++ server/middlewares/modules/cors.middleware.js
@@ -1,8 +1,10 @@
 const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 
 // Middleware instance for CORS
 const corsMiddleware = cors({
-  origin: "https://anime-alley-beige.vercel.app",
+  origin: process.env.CLIENT_URL,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
