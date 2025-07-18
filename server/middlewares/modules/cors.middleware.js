55a26286631a60d39c7715a9abbf1be649fb4e44
Abdullah <abdullahdeveloper843@gmail.com>
2025-07-18 21:35:23 +0500

    allowed origins for cors were added

--- server/middlewares/modules/cors.middleware.js
+++ server/middlewares/modules/cors.middleware.js
@@ -11,7 +11,7 @@ const allowedOrigins = [
 
 // Simple CORS middleware
 const corsMiddleware = cors({
-  origin: true,
+  origin: allowedOrigins,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
