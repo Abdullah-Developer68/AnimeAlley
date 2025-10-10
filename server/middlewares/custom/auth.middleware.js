16f692ec128f7b24865952d9ad3eb78795838115
Abdullah <abdullahdeveloper843@gmail.com>
2025-10-10 16:06:45 +0500

    A middleware was created to verify the token on incoming requests it also extracts the user data from the token and attaches it with the request. Now, this has been created so modify APIS to not send user data along with the request that was for protoyping only

---
+++ server/middlewares/custom/auth.middleware.js
@@ -0,0 +1,55 @@
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const verifyTokenMiddleware = async (req, res, next) => {
+  try {
+    // Extract token from Authorization header or cookies
+    let token = null;
+
+    const authHeader = req.headers.authorization;
+    if (authHeader && authHeader.startsWith("Bearer ")) {
+      // Extracts the String (token) after 7th index which is after "Bearer "
+      token = authHeader.substring(7);
+    } else if (req.cookies && req.cookies.token) {
+      token = req.cookies.token;
+    }
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Authentication failed. No token provided",
+      });
+    }
+
+    // verify token
+    const decoded = jwt.verify(token, process.env.JWT_KEY);
+
+    // Attach the decoded data (userInfo) to the request object
+    req.user = {
+      userid: decoded.userid,
+      email: decoded.email,
+      username: decoded.username,
+      role: decoded.role,
+      profilePic: decoded.profilePic,
+    };
+    // Proceed to the next middleware or route handler
+    next();
+  } catch (error) {
+    console.error("Error verifying token:", error);
+
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({ success: false, message: "Token expired" });
+    }
+
+    if (error.name === "JsonWebTokenError") {
+      return res.status(401).json({ success: false, message: "Invalid token" });
+    }
+
+    return res
+      .status(500)
+      .json({ success: false, message: "Internal server error" });
+  }
+};
+
+module.exports = verifyTokenMiddleware;
